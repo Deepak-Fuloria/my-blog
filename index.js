bed11d254317e5079929d19dbc3cc18bbac90e7a
Deepak Fuloria <fuloriadeepak999@gmail.com>
2023-03-07 16:31:30 +0530

    test

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 const express=require("express")
 const app=express()
 const mongoose=require("mongoose")
-const bodyParser = require('body-parser')
-require("dotenv").config()
-const connect=require("./models/db")        //connection with mongo
-// const PORT=process.env.PORT || 3000;
-connect();                                 //connection with mongo
-const postRoutes=require("./routes/postsRoutes")
-const path=require("path")
+// const bodyParser = require('body-parser')
+// require("dotenv").config()
+// const connect=require("./models/db")        //connection with mongo
+// // const PORT=process.env.PORT || 3000;
+// connect();                                 //connection with mongo
+// const postRoutes=require("./routes/postsRoutes")
+// const path=require("path")
 
-var session = require('express-session');
+// var session = require('express-session');
 
-var MongoDBStore = require('connect-mongodb-session')(session);
-const flash=require("express-flash")
-const userRoutes=require("./routes/userRoutes")
-const profileRoutes=require("./routes/profileRoutes")
+// var MongoDBStore = require('connect-mongodb-session')(session);
+// const flash=require("express-flash")
+// const userRoutes=require("./routes/userRoutes")
+// const profileRoutes=require("./routes/profileRoutes")
 
 
-var store = new MongoDBStore({
-  uri: process.env.DB,
-  collection: 'mySessions'
-});
+// var store = new MongoDBStore({
+//   uri: process.env.DB,
+//   collection: 'mySessions'
+// });
 
-app.use(require('express-session')({
-  secret: 'This is a secret',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
-  },
-  store: store,
-  resave: true,
-  saveUninitialized: true
-}));
+// app.use(require('express-session')({
+//   secret: 'This is a secret',
+//   cookie: {
+//     maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+//   },
+//   store: store,
+//   resave: true,
+//   saveUninitialized: true
+// }));
 
-app.use(flash())
-app.use((req,res,next)=>{
- res.locals.message=req.flash()
- next();
-})
-app.use(express.static(path.join(__dirname,"./views")))
-// console.log("reached here",path.join(__dirname,"./views"))
-// app.use(express.static(path.join(__dirname,"./client/build")))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// app.use(flash())
+// app.use((req,res,next)=>{
+//  res.locals.message=req.flash()
+//  next();
+// })
+// app.use(express.static(path.join(__dirname,"./views")))
+// // console.log("reached here",path.join(__dirname,"./views"))
+// // app.use(express.static(path.join(__dirname,"./client/build")))
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
-app.set("view engine","ejs")
+// app.set("view engine","ejs")
 
-app.use(userRoutes)
+// app.use(userRoutes)
 // app.use(profileRoutes)
 // app.use(postRoutes)
-
+app.get("/",(req,res)=>{
+ res.send("done")
+})
 app.listen(process.env.PORT || 3000,()=>{
     console.log(`server is running on port no ${process.env.PORT || 3000}`)
    
