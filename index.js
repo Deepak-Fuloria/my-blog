7aa6921c3c77c068b2718ff0201e72b0c45dcc50
Deepak Fuloria <fuloriadeepak999@gmail.com>
2023-03-07 15:56:05 +0530

    index change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connect=require("./models/db")        //connection with mongo
 const PORT=process.env.PORT || 3000;
 connect();                                 //connection with mongo
 const postRoutes=require("./routes/postsRoutes")
-
+const path=require("path")
 
 var session = require('express-session');
 
@@ -37,7 +37,9 @@ app.use((req,res,next)=>{
  res.locals.message=req.flash()
  next();
 })
-app.use(express.static("./views"))
+app.use(express.static(path.join(__dirname,"./views")))
+// console.log("reached here",path.join(__dirname,"./views"))
+// app.use(express.static(path.join(__dirname,"./client/build")))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
